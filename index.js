dd19186f4373e7e174ed368f6f0b10150944c48f
King <rey24vegas@gmail.com>
2025-09-29 20:17:31 -0300

    Actualizada la base de datos

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 import express from 'express'
+import pool from './config/conexion.js'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('API_REST_FUL__MYSQL')
 })
-app.post('/', (req, res) => {
+
+app.get('/users', async (req, res) => {
+    const sql = 'select * from users'
+    try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql)
+        connection.release()
+        res.json(rows)
+    } catch(error){
+        res.status(500).send('Error con la consulta')
+    }
+})
+
+app.get('/users/:id', async (req,res) => {
+    const id = req.params.id
+    const sql = 'select * from users where ID_User = ?'
+    try{
+        const connection = await pool.getConnection()
+        const [rows] = await connection.query(sql, [id])
+        connection.release()
+        res.json(rows)
+    } catch(error){
+        res.status(500).send('Error con la consulta')
+    }
+})
+
+app.post('/users', (req, res) => {
     
 })
-app.put('/', (req, res) => {
+app.put('/users', (req, res) => {
     
 })
-app.delete('/', (req, res) => {
+app.delete('/users', (req, res) => {
     
 })
 app.use((req,res) =>{
     res.status(404).send('Pagina Inexistente')
 })
-app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${PORT}`))
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
